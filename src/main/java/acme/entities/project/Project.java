25924a80aef22304ef18d63c7406f3777c023263
vicgrabru <vgravanbru@gmail.com>
2024-02-18 18:07:09 +0100

    Task-006-S1: Project Entity Done

---
+++ src/main/java/acme/entities/project/Project.java
@@ -0,0 +1,62 @@
+
+package acme.entities.project;
+
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.PositiveOrZero;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Project extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long		serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Column(unique = true)
+	@NotBlank
+	@Pattern(regexp = "[A-Z]{3}-[0-9]{4}", message = "{validation.project.code}")
+	private String					code;
+
+	@NotBlank
+	@Length(max = 75)
+	private String					title;
+
+	@NotBlank
+	@Length(max = 100)
+	private String					abstractField;
+
+	@NotNull
+	private boolean					hasFatalErrors;
+
+	@NotNull
+	@PositiveOrZero
+	private Integer					cost;
+
+	@URL
+	private String					optionalLink;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@OneToMany
+	private Collection<UserStory>	userStories;
+
+}
